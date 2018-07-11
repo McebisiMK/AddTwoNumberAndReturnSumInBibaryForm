d2272de5071b3808464699c5983f789cf38f37ad
MCEBISI MKHOHLIWE <mcebisimkhohliwe@gmail.com>
2018-07-11 09:34:25 +0200

    Working solution

---
+++ AddTwoNumbersAndReturnSumInBinary/SumOfTwoNumbersInBibary_Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbersAndReturnSumInBinary
+{
+    [TestFixture]
+    public class SumOfTwoNumbersInBibary_Tests
+    {
+        [TestCase(1, 2, "11")]
+        [TestCase(11, 12, "10111")]
+        [TestCase(49, 51, "1100100")]
+        public void GetBinaryFormOfSumOfTwoNumbers_GivenTwoNumberShouldAddThemAndReturnBinaryFormOfSum(int firstNumber, int secondNumber,string expected)
+        {
+            //---------------------------Arrange----------------------------
+            var sumOfTwoNumbersInBibary = new SumOfTwoNumbersInBinary();
+
+            //---------------------------Act--------------------------------
+            var actual = sumOfTwoNumbersInBibary.GetBinaryFormOfSumOfTwoNumbers(firstNumber, secondNumber);
+
+            //---------------------------Assert-----------------------------
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ AddTwoNumbersAndReturnSumInBinary/SumOfTwoNumbersInBinary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AddTwoNumbersAndReturnSumInBinary
+{
+    public class SumOfTwoNumbersInBinary
+    {
+        public string GetBinaryFormOfSumOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            var sumOfTwoNumbers = firstNumber + secondNumber;
+            var binaryForm = new List<string>();
+            while(sumOfTwoNumbers > 0)
+            {
+                var remainder = sumOfTwoNumbers % 2;
+                sumOfTwoNumbers = (sumOfTwoNumbers - remainder) / 2;
+                binaryForm.Add(remainder.ToString());
+            }
+            binaryForm.Reverse();
+            return $"{string.Join("", binaryForm)}";
+            //This can do it but I was focusing on understanding the whole process(logic)
+            //return Convert.ToString(firstNumber + secondNumber, 2);
+        }
+    }
+}
